15bc41110a9220001936f0befb54c961cab0a93d
Hazmad <85420204+Hazmadd@users.noreply.github.com>
2023-06-04 15:05:06 -0500

    v1.2.1

--- AntiInsideTerrainViolation.cs
+++ AntiInsideTerrainViolation.cs
@@ -8,7 +8,7 @@ using Oxide.Ext.DiscordWebhooks;
 
 namespace Oxide.Plugins
 {
-    [Info("AntiInsideTerrainViolation", "Hazmad", "1.0.0")]
+    [Info("AntiInsideTerrainViolation", "Hazmad", "1.2.1")]
     [Description("Teleports players to a safe location when they violate antihack InsideTerrain.")]
     class AntiInsideTerrainViolation : RustPlugin
     {
