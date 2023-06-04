52491a6a22913364eedf3de9f25d90719d2b1b2d
Hazmad <85420204+Hazmadd@users.noreply.github.com>
2023-06-04 14:52:16 -0500

    v1.2.0
    Handle antihack violations instead of RustEdit terraintriggers

--- AntiInsideTerrainViolation.cs
+++ AntiInsideTerrainViolation.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Oxide.Core;
 using Oxide.Core.Configuration;
@@ -8,7 +9,7 @@ using Oxide.Ext.DiscordWebhooks;
 namespace Oxide.Plugins
 {
     [Info("AntiInsideTerrainViolation", "Hazmad", "1.0.0")]
-    [Description("Teleports players to a safe location when they violate InsideTerrain.")]
+    [Description("Teleports players to a safe location when they violate antihack InsideTerrain.")]
     class AntiInsideTerrainViolation : RustPlugin
     {
         private DynamicConfigFile config;
@@ -17,16 +18,22 @@ namespace Oxide.Plugins
 
         private string chatMessage;
         private string consoleLogMessage;
+        private bool useDiscordWebhook;
+        private string discordWebhookURL;
         private string discordWebhookMessage;
 
         protected override void LoadDefaultConfig()
         {
             Config["SafeLocation"] = "0 0 0";
-            Config["DiscordWebhookURL"] = "";
+            Config["UseDiscordWebhook"] = false;
 
-            Config["ChatMessage"] = "You fell into an unsafe area of the map and have been teleported to a safe location.";
-            Config["ConsoleLogMessage"] = "InsideTerrain violation: Player '{player}' ({playerID}) was teleported to a safe location.";
-            Config["DiscordWebhookMessage"] = "InsideTerrain Violation\nPlayer: {player} ({playerID})\nViolation Location: {position}";
+            Config["ChatMessage"] =
+                "Invalid terrain entry! You have been relocated to a secure area.";
+            Config["ConsoleLogMessage"] =
+                "Antihack violation: Player '{player}' ({playerID}) was teleported to a safe location. Violation Location: {position}";
+            Config["DiscordWebhookURL"] = "";
+            Config["DiscordWebhookMessage"] =
+                "Antihack Violation\nPlayer: {player} ({playerID})\nViolation Location: {position}";
 
             SaveConfig();
         }
@@ -44,6 +51,8 @@ namespace Oxide.Plugins
             try
             {
                 safeLocation = GetConfig<Vector3>("SafeLocation");
+                useDiscordWebhook = GetConfig<bool>("UseDiscordWebhook");
+                discordWebhookURL = GetConfig<string>("DiscordWebhookURL");
 
                 chatMessage = GetConfig<string>("ChatMessage");
                 consoleLogMessage = GetConfig<string>("ConsoleLogMessage");
@@ -57,9 +66,11 @@ namespace Oxide.Plugins
 
             if (safeLocation == Vector3.zero)
             {
-                Puts("Attention! Be aware that you have not specified a default secure location in the configuration file. " +
-                      "Unless you have a deliberate intention for this, it could lead to players being unexpectedly teleported to hazardous areas. " +
-                      "It is essential to input appropriate coordinates for a safe zone to prevent any potential risks.");
+                Puts(
+                    "Attention! Be aware that you have not specified a default secure location in the configuration file. "
+                        + "Unless you have a deliberate intention for this, it could lead to players being unexpectedly teleported to hazardous areas. "
+                        + "It is essential to input appropriate coordinates for a safe zone to prevent any potential risks."
+                );
             }
         }
 
@@ -79,22 +90,25 @@ namespace Oxide.Plugins
             permission.RegisterPermission("antiinsideterrainviolation.bypass", this);
         }
 
-        void OnEntityEnter(TriggerZone zone, BaseEntity entity)
+        object OnPlayerViolation(BasePlayer player, AntiHackType type)
         {
-            var player = entity as BasePlayer;
-            if (player == null)
-                return;
+            if (
+                player == null
+                || player.IsAdmin
+                || permission.UserHasPermission(
+                    player.UserIDString,
+                    "antiinsideterrainviolation.bypass"
+                )
+            )
+                return null;
 
-            if (zone is TerrainTrigger && zone.Check(entity.WorldSpaceBounds().ToBounds()) == Zone.TestResult.FailInside)
+            if (type == AntiHackType.InsideTerrain)
             {
-                if (player.IsAdmin || permission.UserHasPermission(player.UserIDString, "antiinsideterrainviolation.bypass"))
-                    return;
-
-                if (player.lastSentInsideTerrainDistance >= 200f)
-                {
-                    HandleInsideTerrainViolation(player);
-                }
+                HandleInsideTerrainViolation(player);
+                return true; // Handle default antihack behavior
             }
+
+            return null;
         }
 
         void HandleInsideTerrainViolation(BasePlayer player)
@@ -103,27 +117,29 @@ namespace Oxide.Plugins
 
             player.ChatMessage(chatMessage);
 
-            var logMessage = consoleLogMessage.Replace("{player}", player.displayName).Replace("{playerID}", player.UserIDString);
+            var violationLocation = player.transform.position.ToString();
+
+            var logMessage = consoleLogMessage
+                .Replace("{player}", player.displayName)
+                .Replace("{playerID}", player.UserIDString)
+                .Replace("{position}", violationLocation);
             LogToConsole(logMessage);
 
-            SendDiscordReport(player);
+            if (useDiscordWebhook && !string.IsNullOrEmpty(discordWebhookURL))
+                SendDiscordReport(player, violationLocation);
         }
 
-        void SendDiscordReport(BasePlayer player)
+        void SendDiscordReport(BasePlayer player, string violationLocation)
         {
-            var webhookURL = GetConfig<string>("DiscordWebhookURL");
-            if (string.IsNullOrEmpty(webhookURL))
-                return;
-
             var discordMessage = new DiscordWebhooks.Message
             {
                 content = discordWebhookMessage
                     .Replace("{player}", player.displayName)
                     .Replace("{playerID}", player.UserIDString)
-                    .Replace("{position}", player.transform.position.ToString())
+                    .Replace("{position}", violationLocation)
             };
 
-            discord.SendMessage(webhookURL, discordMessage);
+            discord.SendMessage(discordWebhookURL, discordMessage);
         }
     }
 }
